83cf1d4efb57b14714262457b24e7ebae3ab498b
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-02-02 11:05:06 +0200

    padarytas header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,39 @@
 import { Link, Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import UserContext from '../contexts/UserContext';
+import { useContext } from "react";
+
 
 const Header = () => {
+
+   const { loggedInUser, setLoggedInUser} = useContext(UserContext);
+   const navigation = useNavigate();
+
+   const logOutUser = () => {
+      setLoggedInUser(null);
+      navigation('/login');
+   }
+
    return (
       <>
+      <img src="https://www.pngitem.com/pimgs/m/485-4852378_sample-logo-png-transparent-png.png" alt="logo" />
+      {
+            loggedInUser 
+            ? 
       <div>
          <Link to='/'>Home</Link>
          <Link to='/add'>Add new Post</Link>
+         <button onClick={() => logOutUser()}>Log Out</button>
+         
       </div>
+      :
       <div className="loginRegister">
          <Link to='/login'>Login</Link>
          <Link to='/register'>Register</Link>
       </div>
-         <hr />
+      }
          <Outlet />
+         
       </>
    );
 }
