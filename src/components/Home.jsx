b5e4a9ecf8065807a31251b02e9e3fc8d2cdf1aa
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-02-02 11:43:36 +0200

    sukurtas home and fixed routes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
 const Home = () => {
+   const [posts, setPosts] = useState([]);
+   
+   const fetchFromServer = async () => {
+      const response = await fetch('http://localhost:5000/posts')
+         .then(response => response.json())
+
+      setPosts(response)
+    };
+ 
+    useEffect(() => {
+      fetchFromServer();
+    }, []);
+
    return ( 
-      <></>
-    );
+      <>
+       <div className='posts'>
+         {posts.length === 0 ? (
+            <p>No posts available</p>
+         ) : (
+            posts ? (
+            posts.map((post) => (
+               <div key={post.id} className="post">
+                  <h2>{post.text}</h2>
+                  <p>{post.description}</p>
+               </div>
+            ))
+         ) : (
+            <img src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif" alt="loading"/>
+         )
+      )}
+   </div>
+     </>
+   );
 }
  
 export default Home;
