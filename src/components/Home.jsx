14f0cb66ae53e2fa157ac157c5ca20f2944467db
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-02-03 07:55:57 +0200

    home css pridetas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import UserContext from '../contexts/UserContext';
+import './styles/home.css'
 
 
 const Home = () => {
@@ -33,7 +34,9 @@ const Home = () => {
 
    return ( 
       <>
+      <div className='home'>
       {renderPosts()}
+      </div>
       </>
    );
 }
